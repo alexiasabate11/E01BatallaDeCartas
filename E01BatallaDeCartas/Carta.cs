f3464b71751c2ef1274bd6b278f867c0e1e43966
alexiasabate11 <alexiablackha@gmail.com>
2024-09-18 16:15:50 +0200

    Clase Carta creada

--- E01BatallaDeCartas/Carta.cs
+++ E01BatallaDeCartas/Carta.cs
@@ -8,5 +8,35 @@ namespace E01BatallaDeCartas
 {
     internal class Carta
     {
+        public enum ePalos
+        {
+            Oros,
+            Picas,
+            Corazones,
+            Diamantes
+        }
+        
+        private int numero;
+        private ePalos palo;
+
+        public Carta() { }
+
+        public int Numero
+        {
+            get { return numero; }
+            set { numero = value; }
+        }
+
+        public ePalos Palo
+        {
+            get { return palo; }
+            set { palo = value; }
+        }
+
+        public Carta(int numero, ePalos palo) 
+        {
+            this.numero = numero;
+            this.palo = palo;
+        }
     }
 }
